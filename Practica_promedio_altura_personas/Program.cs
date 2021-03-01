a5603e9ec8677687e37757575fc2ef092496b404
GJAA98 <79035604+GJAA98@users.noreply.github.com>
2021-03-01 16:59:44 -0400

    Add files via upload
    Crear un programa que defina un array de 5 elementos de tipo float que representen las alturas de 5 personas.
    Obtener el promedio de las mismas. Contar cuántas personas son más altas que el promedio y cuántas más bajas.

---
+++ Practica_promedio_altura_personas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Practica_promedio_altura_personas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] numeros = new float[5];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("Inserte un numero:");
+                string valor = Console.ReadLine();
+                numeros[i] = float.Parse(valor);
+            }
+            
+            float sumatoria = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {                
+                sumatoria += numeros[i];
+            }
+            float media = sumatoria / numeros.Length;
+
+            int masAltas = 0;
+            int masBajas = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] > media)
+                {
+                    masAltas++;
+                }
+                else
+                {
+                    masBajas++;
+                }
+            }
+            Console.WriteLine("Promedio de altura:" + media);
+            Console.WriteLine("Personas mas altas:" + masAltas);
+            Console.WriteLine("Personas mas bajas:" + masBajas);
+        }
+    }
+}
